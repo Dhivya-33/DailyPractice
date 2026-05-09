1d44e540faf6ee07586a0c9c7a4b51d22e953dc6
Dhivya Rajasundaram <dhivyar2803@gmail.com>
2026-05-09 18:32:58 +0530

    daily update

---
+++ Java/Minimumsizesubarraysum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Minimumsizesubarraysum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int target=sc.nextInt();
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int i=0;
+        int sum=0;
+        int count=0;
+        int min=Integer.MAX_VALUE;
+        while(arr[i]<target){
+            sum+=arr[i];
+            count++;
+            if(sum==target){
+                min=Math.min(min,count);
+            }else{
+                i++;
+            }
+        }
+        System.out.println(min);
+    }
+}
